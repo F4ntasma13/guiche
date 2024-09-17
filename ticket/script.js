831e7e6fa51436ae7630c9ba639fe4b223982695
Pedro <nmsystem@gmail.com>
2024-09-17 11:36:12 -0300

    concluido botões de atendimento guiché

--- ticket/script.js
+++ ticket/script.js
@@ -1,62 +1,68 @@
 var contadorG = 0
 var contadorP = 0
 var contadorI = 0
-var atendimentoG = []
-var atendimentoI = []
-var atendimentoP = []
+var tickets = []
 var clientesAtendidos = []
+
 const deslogar = () => location.reload()
 
 document.getElementById("atendimento").innerHTML = "Status: Deslogado",
 document.getElementById("atendimento2").innerHTML = "Status: Deslogado",
-document.getElementById("atendimento3").innerHTML = "Status: Deslogado"       
+document.getElementById("atendimento3").innerHTML = "Status: Deslogado"
 
 
 
-const gerarTicket = (ticket, prefixo) =>{
+
+const logar = () => {
+    let iniciar = 0
+    if(iniciar === 0){
+        document.getElementById("botao-container").innerHTML = 
+        "<button class='botoes' onclick='guicheOne()' id= 'atenderBtn' disabled>Atender</button>"
+        document.getElementById("botao-container2").innerHTML = 
+        "<button class='botoes' onclick='guicheTwo()' id= 'atenderBtn' disabled>Atender</button>"
+        document.getElementById("botao-container3").innerHTML = 
+        "<button class='botoes' onclick='guicheTree()' id= 'atenderBtn' disabled>Atender</button>"
+    }
+    document.getElementById("atendimento").innerHTML = "Status: logado"
+    document.getElementById("atendimento2").innerHTML = "Status: logado"
+    document.getElementById("atendimento3").innerHTML = "Status: logado"  
+}
+
+const gerarTicket = (ticket) =>{
     let lista = ""
     for(let tick = 0; tick < ticket.length; tick++){
-        lista += "<li>" + prefixo + ticket[tick] + "</li>"
+        lista += "<li>" + ticket[tick] + "</li>"
+    }
+    let botoes = document.getElementsByClassName("botoes")
+    for(let d = 0; d < botoes.length; d++){
+        botoes[d].disabled = false
     }
     return lista
-}
-const ticketGeral = (atendimento, prefixo) => {
-    listaGr = ""
+    
+};
+const ticketGeral = () => {
     contadorG++
-    let numeroAtendimento = contadorG.toString()
-    atendimento.push(numeroAtendimento)
+    let numeroAtendimento = "GR00" + contadorG.toString()
 
-    listaGr += gerarTicket(atendimentoG, prefixo)    
-    document.getElementById("senha").innerHTML = listaGr
-}
-const ticketIdoso = (atendimento, prefixo) => {
-    listaId = ""
-    contadorI++
-    let numeroAtendimentoI = contadorI.toString()
-    atendimento.push(numeroAtendimentoI)
+    tickets.push(numeroAtendimento)
+    document.getElementById("senhas").innerHTML = gerarTicket(tickets)
 
-    listaId += gerarTicket(atendimentoI, prefixo)  
-    document.getElementById("idoso").innerHTML = listaId
-
-}
-const ticketPreferencial = (atendimento, prefixo) => {
-    listaPr = ""
+};
+const ticketIdoso = () => {
+    contadorI++  
+    let numeroAtendimento = "ID00" + contadorI.toString();
+  
+    tickets.push(numeroAtendimento)
+    document.getElementById("senhas").innerHTML = gerarTicket(tickets)
+};
+const ticketPreferencial = () => {
     contadorP++
-    let numeroAtendimentoP = contadorP.toString()
-    atendimento.push(numeroAtendimentoP)
+    let numeroAtendimento = "PR00" + contadorP.toString()
 
-    listaPr += gerarTicket(atendimentoP, prefixo)  
-    document.getElementById("preferencial").innerHTML = listaPr
-}
-const geral = () => {
-    ticketGeral(atendimentoG, "GR20");
-}
-const preferencial = () => {
-    ticketPreferencial(atendimentoP, "PR10")
-}
-const idoso = () => {
-    ticketIdoso(atendimentoI, "ID00")
-}
+    tickets.push(numeroAtendimento)
+    document.getElementById("senhas").innerHTML = gerarTicket(tickets)
+};
+
 const guicheOne = () => {
     atendimentos("atendimento")
     const atenderBtn = document.getElementById("botao-container");
@@ -67,95 +73,130 @@ const guicheTwo = () => {
     const atenderBtn = document.getElementById("botao-container2");
     atenderBtn.innerHTML = "<button id='finalizarBtn' onclick='finalizarAtendimentoDois()'>Finalizar Atendimento</button>"
 }
-
 const guicheTree = () => {
     atendimentos("atendimento3")
     const atenderBtn = document.getElementById("botao-container3");
     atenderBtn.innerHTML = "<button id='finalizarBtn' onclick='finalizarAtendimentoTres()'>Finalizar Atendimento</button>"
+
 }
 
 const atendimentos = (elementId) => {
-    let atnGeral = ""
-    
-    if (atendimentoI.length > 0) {
-        let atendidos = atendimentoI[0]
-        atnGeral += "<li>ID00" + atendidos + "</li>"
-        
-        for (let id = 0; id < atendimentoI.length - 1; id++) {
-            atendimentoI[id] = atendimentoI[id + 1]
-        }
-        atendimentoI.length = atendimentoI.length - 1
-        clientesAtendidos.push("ID00" + atendidos)
-    }
-    else if (atendimentoP.length > 0) {
-        let atendidos = atendimentoP[0]
-        atnGeral += "<li>PR10" + atendidos + "</li>"
+    let atnGeral = "";
+    let emAtendido = "";
+    let clientesEmAtendimento = [];
 
-        for (let pr = 0; pr < atendimentoP.length - 1; pr++) {
-            atendimentoP[pr] = atendimentoP[pr + 1]
-        }
-        atendimentoP.length = atendimentoP.length - 1
-        clientesAtendidos.push("PR10" + atendidos)
-    }
-    else if (atendimentoG.length > 0) {
-        let atendidos = atendimentoG[0]
-        atnGeral += "<li>GR20" + atendidos + "</li>"
 
-        for (let gr = 0; gr < atendimentoG.length - 1; gr++) {
-            atendimentoG[gr] = atendimentoG[gr + 1]
+    if (tickets.length > 0) {
+        for (let atn = 0; atn < tickets.length; atn++) {
+            if (tickets[atn].indexOf("ID") !== -1 && !emAtendido) {
+                emAtendido = tickets[atn];
+            } else {
+                clientesEmAtendimento.push(tickets[atn]);
+            }
         }
-        atendimentoG.length = atendimentoG.length - 1
-        clientesAtendidos.push("GR20" + atendidos)
+        if (!emAtendido) {
+            for (let atn = 0; atn < clientesEmAtendimento.length; atn++) {
+                if (clientesEmAtendimento[atn].indexOf("PR") !== -1 && !emAtendido) {
+                    emAtendido = clientesEmAtendimento[atn];
+                    clientesEmAtendimento.splice(atn, 1)
+                    break;
+                }
+            }
+        }
+        if (!emAtendido) {
+            emAtendido = tickets[0]
+            clientesEmAtendimento = tickets.slice(1)
+        }
+
+        atnGeral += "<li>" + emAtendido + "</li>";
+
+        tickets.length = 0;
+        for (let i = 0; i < clientesEmAtendimento.length; i++) {
+            tickets.push(clientesEmAtendimento[i]);
+        }
+        document.getElementById("senhas").innerHTML = gerarTicket(tickets);
+
     }
+
     document.getElementById(elementId).innerHTML = atnGeral;
+
 }
+
 const finalizarAtendimento = () => {
     document.getElementById("botao-container").innerHTML = 
     "<button id= 'atenderBtn' onclick='guicheOne()'>Atender</button>"
     mostrarAtendidos("concluido")   
+    if(!tickets.length){
+        document.getElementById("botao-container").innerHTML =
+        "<button class='botoes' onclick='guicheOne()' disabled id= 'atenderBtn'>Atender</button>"
+    }
+
     document.getElementById("atendimento").innerHTML = "Status: disponivel"
 }
 const finalizarAtendimentoDois = () => {
     document.getElementById("botao-container2").innerHTML = 
     "<button id= 'atenderBtn' onclick='guicheTwo()'>Atender</button>"
-    mostrarAtendidos("concluido")   
+    mostrarAtendidosDois("concluido")
+    if(!tickets.length){
+        console.log("tickets.length >= 0: ", tickets.length >= 0);
+        document.getElementById("botao-container2").innerHTML =
+        "<button class='botoes' onclick='guicheTwo()' disabled id= 'atenderBtn'>Atender</button>"
+    }   
     document.getElementById("atendimento2").innerHTML = "Status: disponivel"
 }
 const finalizarAtendimentoTres = () => {
     document.getElementById("botao-container3").innerHTML = 
-    "<button id= 'atenderBtn' onclick='guicheTree()'>Atender</button>"
-    mostrarAtendidos("concluido")   
+    "<button class='botoes' onclick='guicheTree()' id= 'atenderBtn'>Atender</button>"
+    mostrarAtendidosTres("concluido")  
+    if(!tickets.length){
+        document.getElementById("botao-container3").innerHTML =
+        "<button class='botoes' onclick='guicheTree()' disabled id= 'atenderBtn'>Atender</button>"
+    }
+ 
     document.getElementById("atendimento3").innerHTML = "Status: disponivel"
-    // let ticketGr = document.getElementById("senha")
-    // let ticketId = document.getElementById("idoso")
-    // let ticketPr = document.getElementById("preferencial")
 
-    // let listaTicketsGr = ticketGr.getElementsByTagName("li")
-   
-    // if (listaTicketsGr.length > 0 ) {
-    //     ticketGr.removeChild(listaTicketsGr[0])
-    // }
 
 }
 const mostrarAtendidos = (elementId) => {
     let listaAtendidos = "";
-    for (let concluido = 0; concluido < clientesAtendidos.length; concluido++) {
-        listaAtendidos += "<li>" + clientesAtendidos[concluido] + "</li>"
+    let ticketGuiche1 = document.getElementById("atendimento").innerText;
+
+    if (ticketGuiche1 !== "Status: disponivel") {
+        clientesAtendidos.push(ticketGuiche1)
     }
-    document.getElementById(elementId).innerHTML = listaAtendidos
-}
-const logar = () => {
-    let iniciar = 0
-    if(iniciar === 0){
-        document.getElementById("botao-container").innerHTML = 
-            "<button id= 'atenderBtn' onclick='guicheOne()'>Atender</button>"
-        document.getElementById("botao-container2").innerHTML = 
-            "<button id= 'atenderBtn' onclick='guicheTwo()'>Atender</button>"
-        document.getElementById("botao-container3").innerHTML = 
-            "<button id= 'atenderBtn' onclick='guicheTree()'>Atender</button>"
+
+    for (let i = 0; i < clientesAtendidos.length; i++) {
+        listaAtendidos += "<li>" + clientesAtendidos[i] + "</li>";
     }
-    document.getElementById("atendimento").innerHTML = "Status: logado"
-    document.getElementById("atendimento2").innerHTML = "Status: logado"
-    document.getElementById("atendimento3").innerHTML = "Status: logado"
-    
-}
+
+    document.getElementById(elementId).innerHTML = listaAtendidos;
+};
+
+const mostrarAtendidosDois = (elementId) => {
+    let listaAtendidos = "";
+    let ticketGuiche2 = document.getElementById("atendimento2").innerText
+
+    if (ticketGuiche2 !== "Status: disponivel") {
+        clientesAtendidos.push(ticketGuiche2)
+    } 
+
+    for (let i = 0; i < clientesAtendidos.length; i++) {
+        listaAtendidos += "<li>" + clientesAtendidos[i] + "</li>";
+    }
+
+    document.getElementById(elementId).innerHTML = listaAtendidos;
+};
+
+const mostrarAtendidosTres = (elementId) => {
+    let listaAtendidos = "";
+    let ticketGuiche3 = document.getElementById("atendimento3").innerText;
+
+    if (ticketGuiche3 !== "Status: disponivel") {
+        clientesAtendidos.push(ticketGuiche3)
+    }
+    for (let i = 0; i < clientesAtendidos.length; i++) {
+        listaAtendidos += "<li>" + clientesAtendidos[i] + "</li>";
+    }
+
+    document.getElementById(elementId).innerHTML = listaAtendidos;
+};
